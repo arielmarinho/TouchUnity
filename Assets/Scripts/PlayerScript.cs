93e1a1da9a4c8089aa4e55afbb4c72326c7b33a3
logonrm <logonrm@fiap.com.br>
2018-02-17 17:27:51 -0200

    first commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	public float velocidade;
+	public Transform[] posicoes;
+	float direcao_x;
+	float posicaoInicial_x, posicaoFinal_x;
+	int posicaoAtual = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			posicaoInicial_x = Input.mousePosition.x;
+			
+		}
+		else if (Input.GetButtonUp("Fire1")){
+			posicaoFinal_x = Input.mousePosition.x;
+
+			direcao_x = posicaoFinal_x - posicaoInicial_x;
+
+			if (direcao_x > 0 && posicaoAtual < 2) 
+			{
+				posicaoAtual++;
+			} else if(direcao_x < 0 && posicaoAtual > 0) 
+			{
+				posicaoAtual--;
+			}
+			transform.position = Vector3.Lerp (transform.position, posicoes [posicaoAtual].position, 
+				velocidade * Time.deltaTime);	
+			
+		}
+	}
+}
